fa9129e92ed7fa8d2dc706756321a9a1180a3cb6
neutrino-dot <iz24919bd@isk.ed.jp>
2025-07-23 18:43:31 +0900

    一旦消す

--- login.php
+++
@@ -1,8 +0,0 @@
-if ($_POST['username'] == 'admin' && $_POST['password'] == 'secret') {
-  $_SESSION['user'] = 'admin';
-  header('Location: dashboard.php');
-  exit;
-ph} else {
-  header('Location: login.php?error=1');
-  exit;
-}
